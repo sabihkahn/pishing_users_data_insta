3862d832398e38b3f596b5c553a860f7be9a4722
sabih <sabih.dev@gmail.com>
2026-01-08 14:23:00 +0500

    added vercel.json

--- index.js
+++ index.js
@@ -65,7 +65,8 @@ app.post("/getdata",async(req,res)=>{
     }
 })
 
-app.listen(3000,()=>{
-    console.log('server is running on port http://localhost:3000');
+// app.listen(3000,()=>{
+//     console.log('server is running on port http://localhost:3000');
     
-})
+// })
+export default app
